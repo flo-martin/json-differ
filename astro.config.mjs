fa2356247fe5f4a53e17c4139dc2644c752c5da0
Florian Martin <florian.martin@demarque.com>
2026-06-09 11:15:25 -0400

    pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   server: { port: 6700 },
+  site: 'https://flo-martin.github.io',
+  base: '/json-differ',
 });
